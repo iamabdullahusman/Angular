cb06cfc98025ef0d64f1e6a20f9341078fbd01cf
Abdullah Usman <70079106@student.uol.edu.pk>
2023-10-27 15:06:26 +0500

    Component with inline style and template

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,12 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { FormsModule } from '@angular/forms';
+import { UserListComponent } from './user-list/user-list.component';
+import { UserStudentComponent } from './user-student/user-student.component';
+import { CountryListComponent } from './country-list/country-list.component';
+
 
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    UserListComponent,
+    UserStudentComponent,
+    CountryListComponent,
+   
     
   ],
   imports: [
---
+++ src/app/user-student/user-student.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-user-student',
+  template: `
+    <h2 class="custom">
+      user-student works!
+    </h2>
+  `,
+  styleUrls: ['./user-student.component.css']
+})
+export class UserStudentComponent {
+  
+
+}
