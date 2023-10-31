9dfc33380eb81a51c5fc1de32c897b984ad6f1a3
Abdullah Usman <70079106@student.uol.edu.pk>
2023-10-31 11:53:26 +0500

    For Loop

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,7 +7,15 @@ import { NgForm } from '@angular/forms';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title= 'Switch Case ';
- color = "green";
+  title= ' For loop ';
+  users = ['abdullah','ahsan','bilal','anus','mudabir'];
+  userDetails = [
+    {name :'abdullah', email:'abdullah@.com', phone:'1111'},
+    {name :'ahsan', email:'ahsan@.com', phone:'2222'},
+    {name :'bilal', email:'bilal@.com', phone:'3333'},
+    {name :'anus', email:'anus@.com', phone:'4444'},
+    {name :'mudabir', email:'mudabir@.com', phone:'5555'}
+
+  ];
  
 }
