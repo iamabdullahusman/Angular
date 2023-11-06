f95be79129778625d652b949bec8dd381e5d7503
Abdullah Usman <70079106@student.uol.edu.pk>
2023-11-06 15:49:18 +0500

    Directives

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,28 +9,7 @@ import { FormControl , FormGroup , FormControlName , Validators } from '@angular
 })
 
 export class AppComponent {
-  title= ' Angular Template Driven Form';
-  loginForm = new FormGroup({
-    user: new FormControl('',[Validators.required,Validators.email]),
-    password: new FormControl('', [Validators.required,Validators.minLength(5)]),
-
-  })
-loginUser()
-{
-  console.warn(this.loginForm.value);
-}
-get user()
-{
-  return this.loginForm.get('user')
-
-}
-
-get password()
-{
-  return this.loginForm.get('password')
-
-}
+  title= ' Directives in Angular';
   
-
-
+ 
 }
